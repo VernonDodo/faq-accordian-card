6a6d07bf24e1811761795e437cdaa367bb9a8822
VernonDodo <vcdodo@gmail.com>
2021-04-15 16:58:13 -0400

    Styling changes

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let buttons = $(".accordion-button");
+var i;
+
+for (i = 0; i < buttons.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
