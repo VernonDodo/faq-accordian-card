20711c59cd1bb1b648c0df4a3ad975a249505c92
VernonDodo <vcdodo@gmail.com>
2021-04-18 00:06:51 -0400

    Add toggle function to button icon

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
+    this.classList.toggle("collapsed");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
