ab5ce3da6f3077a997e292045f84f11883a5ab3a
VernonDodo <vcdodo@gmail.com>
2021-04-28 02:23:33 -0400

    Fixed a problem with the accordion animation

--- js/main.js
+++ js/main.js
@@ -5,20 +5,12 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     this.classList.toggle("collapsed");
-  });
-}
 
-let buttons = $(".accordion-button");
-var i;
-
-for (i = 0; i < buttons.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = 0;
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.classList.toggle("open")
     }
   });
 }
