cf6cbb0ec205dfa8dacedf477409437a90bf0a97
VernonDodo <vcdodo@gmail.com>
2021-04-27 22:40:38 -0400

    Restored the accordion questions animation

--- js/main.js
+++ js/main.js
@@ -8,17 +8,17 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-// let buttons = $(".accordion-button");
-// var i;
-//
-// for (i = 0; i < buttons.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var panel = this.nextElementSibling;
-//     if (panel.style.maxHeight) {
-//       panel.style.maxHeight = 0;
-//     } else {
-//       panel.style.maxHeight = panel.scrollHeight + "px";
-//     }
-//   });
-// }
+let buttons = $(".accordion-button");
+var i;
+
+for (i = 0; i < buttons.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = 0;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
